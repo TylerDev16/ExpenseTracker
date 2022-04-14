b6b57532ad60f64abc04a6e394bd005ee872267d
TylerDev16 <97607018+TylerDev16@users.noreply.github.com>
2022-04-14 06:44:57 -0400

    Added ExpenseItem component
    Removed hard coded data in jsx to an array of objects. Then displayed the data from array into HTML.
    
    Also moved dummy array data from the ExpenseItem.js component into App.js. Used props to access key value pairs of the data assigned in App.js

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+    // dummy data to replace hard-coded values
+        // const expenseDate = new Date(2021, 2, 28);
+        // const expenseTitle = "Car Insurance";
+        // const expenseAmount = 250.60;
+
+    // output values into HTML code below using {objectName}
+    // for date object, must convert to string using .toISOString() function
+
+    return (
+    <div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
+        </div>
+    </div>
+    );
+}
+
+export default ExpenseItem;
+
+
+// PROPS
+// Used to pass data between components.
+// Pass data to custom component by adding an attribute(props)
+
+// move dummy data from ExpenseItem.js to App.js
+// store data in an array const
+// pass data from dummy data array into App.js ExpenseItem component
+    // ex: <ExpenseItem title={expenses[0].title} --grabs the "title" from the first item/object
+// pass "props" (or whatever name) paramenter into ExpenseItem function
+    // update ExpenseItem html to give access to data. (ex: props.title)
